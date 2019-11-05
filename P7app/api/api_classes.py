9081f25c8b71ac959dc7ff524811557d3cb51e0d
Naomie <naomie.colombo@gmail.com>
2019-11-05 20:16:51 +0100

    test ajax

--- P7app/api/api_classes.py
+++ P7app/api/api_classes.py
@@ -1,6 +1,7 @@
 import requests
+import wikipedia
 
-API_KEY = "AIzaSyDaN2qgNHVhdL7XAaq0SHKhYaqyZV06Ez4"
+API_KEY = "AIzaSyDDyWtczDEZ7MtHtNXVdXcWb7TjFEusJF0"
 class GoogleMapsApi:
 	def __init__(self, place):
 		self.place = place
@@ -15,9 +16,121 @@ class GoogleMapsApi:
 		request = requests.get(self.url, params=parameters)
 		result = request.json()
 		data = []
-		for key, val in result.items():
-			if key == "formatted_address" or key == "location" or key == "status":
-				data.append((key, val))
-				print(data)
-		return result["location"]["lat"]
+		try:
+			data.append(result["results"][0]["formatted_address"])
+			data.append(result["results"][0]["geometry"]["location"]["lat"])
+			data.append(result["results"][0]["geometry"]["location"]["lng"])
+			data.append(result["status"])
+		except KeyError:
+			data = ['Paris, France', 48.856614, 2.3522219, 'OK']
+			print("Résultat par défault!")
+		return data
+
+
+class WikiGobal:
+	"""To set up the WikiApi class"""
+	def __init__(self):
+		self.api_url = "http://en.wikipedia.org/w/api.php" #Default language in english
+
+	def language(self, lang="fr"):
+		"""Set up the language of the requests"""
+		self.api_url = "http://" + lang.lower() + ".wikipedia.org/w/api.php"
 		
+
+	def w_request(self, params):
+		"""Make a request on wikipedia's API"""
+		data = requests.get(self.api_url, params=params)
+		return data.json()
+
+	def w_geosearch(self, latitude, longitude, title=None, results=10, radius=1000):
+		geosearch_params = {
+		'list': 'geosearch',
+		'gsradius': radius,
+		'gscoord': '{0}|{1}'.format(latitude, longitude),
+		'gslimit': results,
+		'format': 'json',
+		'action': 'query',
+		}
+		if title:
+			geosearch_params['titles'] = title
+
+		data_result = self.w_request(geosearch_params)
+		search_pages = data_result['query'].get('pages', None)
+		if search_pages:
+			search_results = [v['title'] for k, v in search_pages.items() if k != '-1']
+		else:
+			search_results = [d['title'] for d in data_result['query']['geosearch']]
+
+		return search_results
+
+	def wiki_summary(self, title, sentence=0, char=0):
+		params = {
+			'prop': 'extracts',
+			'explaintext': '',
+			'titles': title,
+			'format': 'json',
+			'action': 'query',
+		}
+
+		if sentence:
+			params['exsentences'] = sentence
+		elif char:
+			params['exchars'] = char
+		else:
+			params['exintro'] = ''
+
+		request = self.w_request(params)
+		summary = request['query']['pages']
+		for k, v in summary.items():
+			result = v['extract']
+
+		return result
+
+	def get_coordinate(self, title):
+		params = {
+			"action": "query",
+			"format": "json",
+			"titles": title,
+			"prop": "coordinates"
+		}
+		request = self.w_request(params)
+		pages = request['query']['pages']
+		for k, v in pages.items():
+			print("Latitute: " + str(v['coordinates'][0]['lat']))
+			print("Longitude: " + str(v['coordinates'][0]['lon']))
+
+
+
+
+class WikiApi:
+	def __init__(self, latitude, longitude, title):
+		self.latitude = latitude
+		self.longitude = longitude
+		self.title = title
+
+	def wiki_request(self):
+		r = WikiGobal()
+		r.language("fr")
+		data = r.w_geosearch(self.latitude, self.longitude, 
+		title=self.title, results=1, radius=(1000))
+		result = r.wiki_summary(data[0], sentence=0, char=0)
+		return result
+
+	def wiki_coordinates(self):
+		r = WikiGobal()
+		r.language("fr")
+		result = r.get_coordinate(self.title)
+		return result
+
+#Paris = 48.856614, 2.3522219
+#Lyon = 45.764942, 4.898393 
+
+
+if __name__ == "__main__":
+	search = "Paris"
+	test = GoogleMapsApi(search)
+	r_google = test.request() 
+	print(r_google)
+	w_request = WikiApi(r_google[1], r_google[2], search)
+	r = w_request.wiki_request()
+	print(r)
--- P7app/views.py
+++ P7app/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from P7app.api import api_classes
+from P7app.api.api_classes import WikiApi
 
 app = Flask(__name__)
 
@@ -6,6 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test_ajax/', methods=['GET'])
+def test():
+	# -----------------------STEP 4-------------------------
+	# Data are collected from the request and sent to an API
+	# ------------------------------------------------------
+	search=request.form['recherche']
+	test = GoogleMapsApi(search)
+	r_google = test.request() 
+	w_request = WikiApi((r_google[1], r_google[2]), search)
+	r = w_request.wiki_request()
+	return render_template('index.html', r=r, question=search)
+"""
 @app.route('/', methods=['POST'])
 def recherche():
 	if request.method == 'POST': 
@@ -16,3 +30,4 @@ def recherche():
 
 #if __name__ == "__main__":
 #    app.run()
+"""
